60c2fd80d0eca4bcfe5a5e8db0a40007101298ed
Leandro Bororo <Leandro Bororo>
2016-10-16 17:26:18 -0200

    refatoracoes

--- app/src/main/java/com/cadastro/projeto/maquinadeguerra/MainActivity.java
+++ app/src/main/java/com/cadastro/projeto/maquinadeguerra/MainActivity.java
@@ -40,15 +40,16 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
 
-    private final static int REQUEST_ENABLE_BT = 1;
     private static final String MAC_ADDRESS = "98:D3:31:FD:29:75";
+    private final static int REQUEST_ENABLE_BT = 1;
+    private static final int MESSAGE_READ = 1;
+
+    private static final int SUCCESS_CONNECT = 0;
+
     private BluetoothAdapter mBluetoothAdapter;
     private ConnectedThread connectedThread;
-
     private ListView listViewDispositivos;
-    protected static final int SUCCESS_CONNECT = 0;
 
-    protected static final int MESSAGE_READ = 1;
     private Handler mHandler = new Handler(){
 
         private final String TAG = Handler.class.getName();
@@ -60,13 +61,13 @@ public class MainActivity extends AppCompatActivity {
 
             switch(msg.what){
                 case SUCCESS_CONNECT:
-                    // DO something
+                    // Cria e inicia a thread que tratará do envio e recebimento de mensagens
                     makeText(MainActivity.this, "Conectado com sucesso", LENGTH_SHORT).show();
                     connectedThread = new ConnectedThread((BluetoothSocket)msg.obj);
                     connectedThread.start();
-                    Log.i(TAG, "Conectado com sucesso");
                     break;
                 case MESSAGE_READ:
+                    //atualiza o valor retornado pelo sensor na tela
                     String string = (String)msg.obj;
                     TextView view = (TextView) findViewById(R.id.sensorLCD);
                     view.setText(string);
@@ -81,6 +82,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setButtons();
+        obtainBluetoothAdapter();
+        enableBluetoothAdapterAndProgress();
+    }
+
+    /**
+     * Configura as mensagens que serão enviadas ao clicar nos botões
+     */
+    private void setButtons() {
         Button btnF = (Button) findViewById(R.id.btnF);
         btnF.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,10 +114,6 @@ public class MainActivity extends AppCompatActivity {
                 connectedThread.write('S');
             }
         });
-
-
-        obtainBluetoothAdapter();
-        enableBluetoothAdapterAndProgress();
     }
 
     /**
@@ -143,10 +149,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void progress() {
-        findDevice();
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_ENABLE_BT) {
@@ -169,9 +171,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Procura o dispositivo bluetooth do carrinho
+     * Procura o dispositivo bluetooth do carrinho e avança exibindo os dados do carrinho
      */
-    private void findDevice() {
+    private void progress() {
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
 
         if (pairedDevices.size() == 0) {
@@ -222,13 +224,17 @@ public class MainActivity extends AppCompatActivity {
         return null;
     }
 
+    /**
+     * Classe interna que representa a thread que fará a conexão com o dispositivo bia bluetooth
+     */
     private class ConnectThread extends Thread {
 
         private final String TAG = ConnectThread.class.getName();
         private final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
-        private BluetoothSocket mmSocket;;
         private final BluetoothDevice device;
+
+        private BluetoothSocket mmSocket;;
 
         public ConnectThread(BluetoothDevice device) {
             this.device = device;
@@ -240,7 +246,7 @@ public class MainActivity extends AppCompatActivity {
 
             // Usa BluetoothSocket para se conectar a um determinado BluetoothDevice
             try {
-                // MY_UUID é a string UUID da aplicação, também utilizado do lado servidor
+                // Usa BluetoothSocket para se conectar a um determinado BluetoothDevice, utiliza o MY_UUID para identificar esta conexão via socket
                 mmSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
                 Log.e(TAG, "Socket criado com sucesso");
 
@@ -249,7 +255,7 @@ public class MainActivity extends AppCompatActivity {
                     mmSocket.connect();
                     Log.e(TAG, "Conexão estabelecida com sucesso");
 
-                    // Gerencia a conexão (em uma thread separada)
+                    // Envia o socket para outra thread utilizar
                     manageConnectedSocket(mmSocket);
                 } catch (IOException connectException) {
                     try {
@@ -258,10 +264,10 @@ public class MainActivity extends AppCompatActivity {
                         mmSocket =(BluetoothSocket) device.getClass().getMethod("createRfcommSocket", new Class[] {Integer.TYPE}).invoke(device, 1);
                         mmSocket.connect();
                         Log.e(TAG, "Conexão estabelecida com sucesso");
-                        //mmSocket.isConnected()
-                        // Gerencia a conexão (em uma thread separada)
+
+                        // Envia o socket para outra thread utilizar
                         manageConnectedSocket(mmSocket);
-                    } catch (Exception e2) {
+                    } catch (Exception connectException2) {
                         Log.e(TAG, "Não foi possível estabelecer uma conexão");
                     }
                 }
@@ -278,20 +284,23 @@ public class MainActivity extends AppCompatActivity {
                 if (mmSocket != null && mmSocket.isConnected()) {
                     mmSocket.close();
                 }
-            } catch (IOException e) { }
+            } catch (IOException e) {
+                Log.e(TAG, "Não foi possível fechar a conexão");
+            }
         }
 
         /**
-         * Cria uma thread para consumo e envio de dados
+         * Envia mensagem para o Handler com o socket de conexão
          * @param mmSocket BluetoothSocket
          */
         private void manageConnectedSocket(BluetoothSocket mmSocket) {
-            //ConnectedThread
-
             mHandler.obtainMessage(SUCCESS_CONNECT, 10, -1, mmSocket).sendToTarget();
         }
     }
 
+    /**
+     * Classe que representa a thread que ficará de fato responsávl pelo envio e recebimento de mensagens via bluetooth
+     */
     class ConnectedThread extends Thread {
         private final String TAG = ConnectedThread.class.getName();
 
@@ -304,8 +313,6 @@ public class MainActivity extends AppCompatActivity {
             InputStream tmpIn = null;
             OutputStream tmpOut = null;
 
-            // Get the input and output streams, using temp objects because
-            // member streams are final
             try {
                 tmpIn = socket.getInputStream();
                 tmpOut = socket.getOutputStream();
@@ -318,37 +325,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void run() {
-            // buffer para guardar o stream
-            byte[] buffer = new byte[1024];
-
-            // número de bytes retornados pelo método read()
-            int bytes;
-
             while (true) {
                 try {
                     BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
-
                     String line;
+
+                    //envia para o handler as mensagens recebidas pela thread
                     while((line = in.readLine()) != null) {
                         mHandler.obtainMessage(MESSAGE_READ, 10, -1, line).sendToTarget();
                     }
+
                 } catch (IOException e) {
                     Log.e(TAG, "Ocorreu um erro ao receber informações do carrinho");
                 }
             }
         }
 
-        /* Call this from the main activity to send data to the remote device */
+        /**
+         * Método para envio de mensagens
+         * @param comando char representando um comando
+         */
         public void write(char comando) {
             PrintStream ps = new PrintStream(outputStream);
             ps.write(comando);
         }
 
-        /* Call this from the main activity to shutdown the connection */
+        /**
+         * Método para encerramento da conexão
+         */
         public void cancel() {
             try {
-                socket.close();
+                if (socket != null && socket.isConnected()) {
+                    socket.close();
+                }
             } catch (IOException e) {
+                Log.e(TAG, "Não foi possível fechar a conexão");
             }
         }
     }
